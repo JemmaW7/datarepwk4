c43cd6ee577bb5e254442c2926a62f411ce4825c
unknown <g00370688@atu.ie>
2024-10-14 14:20:45 +0000

    lab4 done

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,5 +1,53 @@
-const Create = () => {
-    return <h3>Hello from the Create component</h3>;
-  };
-  
-  export default Create;
+// create.js
+
+import { useState } from "react";
+
+function Create() {
+  const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [poster, setPoster] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+    console.log('Year:', year);
+    console.log('Poster URL:', poster);
+  }
+
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Add Movie Year: </label>
+          <input
+            type="text"
+            className="form-control"
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Add Movie Poster URL: </label>
+          <input
+            type="url"
+            className="form-control"
+            value={poster}
+            onChange={(e) => setPoster(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
+  );
+}
+
+export default Create;
